c1e6127c6f1fd24ab62e326bf4e82bc6034182de
Yoshio HANAWA <y@hnw.jp>
2016-07-07 09:40:58 +0900

    Initial commit

---
+++ PingCmd/PingCmd.h
@@ -0,0 +1,19 @@
+//
+//  PingCmd.h
+//  PingCmd
+//
+//  Created by hnw on 2016/07/07.
+//  Copyright © 2016年 hnw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PingCmd.
+FOUNDATION_EXPORT double PingCmdVersionNumber;
+
+//! Project version string for PingCmd.
+FOUNDATION_EXPORT const unsigned char PingCmdVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PingCmd/PublicHeader.h>
+
+
---
+++ PingCmdTests/PingCmdTests.swift
@@ -0,0 +1,36 @@
+//
+//  PingCmdTests.swift
+//  PingCmdTests
+//
+//  Created by hnw on 2016/07/07.
+//  Copyright © 2016年 hnw. All rights reserved.
+//
+
+import XCTest
+@testable import PingCmd
+
+class PingCmdTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
